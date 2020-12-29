02d47c3ef14c5e984c6a5cb71194d18fcf9f2a3f
Rogério Toledo <rotoledojr@gmail.com>
2020-12-29 19:31:03 -0300

    Setup inicial
    Setup inicial do projeto

---
+++ Desafio M2Y/Desafio M2Y/Desafio_M2YApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_M2YApp.swift
+//  Desafio M2Y
+//
+//  Created by Rogério Toledo on 29/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_M2YApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
